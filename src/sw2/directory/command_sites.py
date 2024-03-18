b05e85f7eba64249a94614354fe3b52ded16c639
Miki Yutani <mkyutani@gmail.com>
2024-03-18 14:22:38 +0900

    Print directory id & name, and remove --sort

--- src/sw2/directory/command_sites.py
+++ src/sw2/directory/command_sites.py
@@ -11,12 +11,10 @@ def sw2_parser_directory_sites(subparser):
     parser.add_argument('--delimiter', nargs=1, default=[' '], help='delimiter')
     parser.add_argument('--json', action='store_true', help='in json format')
     parser.add_argument('--strict', action='store_true', help='strict name check')
-    parser.add_argument('--sort', action='store_true', help='sort by name')
 
 def sw2_directory_sites(args):
     args_name = args.get('name')
     args_strict = args.get('strict')
-    args_sort = args.get('sort')
     args_json = args.get('json')
     args_delimiter = args.get('delimiter')[0]
 
@@ -36,13 +34,10 @@ def sw2_directory_sites(args):
     for id in ids:
         sites.extend(get_sites_by_directory(id))
 
-    if args_sort:
-        sites.sort(key=lambda x: x['name'])
-
     if args_json:
         print(json.dumps(sites))
     else:
         for site in sites:
-            print(str(site['id']), site['name'], site['uri'], sep=args_delimiter)
+            print(str(site['directory']), site['directory_name'], str(site['id']), site['name'], site['uri'], sep=args_delimiter)
 
     return 0
