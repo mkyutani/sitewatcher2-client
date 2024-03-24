845b680c194b177f8d2b85c0f5008f6a3653946e
Miki Yutani <mkyutani@gmail.com>
2024-03-24 13:57:12 +0900

    Initial update in add

--- src/sw2/site/command_add.py
+++ src/sw2/site/command_add.py
@@ -1,34 +1,32 @@
 import json
 import sys
-from urllib.parse import urljoin
 import requests
 from sw2.directory.list import get_directories
 from sw2.env import Environment
-from sw2.util import is_uuid
+from sw2.site.update import update_site_resources
+from sw2.site.list import get_sites
 
 def sw2_parser_site_add(subparser):
     parser = subparser.add_parser('add', help='add site')
     parser.add_argument('directory', metavar='DIR', help='directory id or name')
     parser.add_argument('name', metavar='NAME', help='name')
     parser.add_argument('uri', metavar='URI', help='source uri')
+    parser.add_argument('--delimiter', nargs=1, default=[' '], help='delimiter')
+    parser.add_argument('--json', action='store_true', help='in json format')
 
 def sw2_site_add(args):
     args_directory = args.get('directory')
     args_name = args.get('name')
     args_uri = args.get('uri')
+    args_delimiter = args.get('delimiter')[0]
+    args_json = args.get('json')
 
-    if is_uuid(args_directory):
-        directory = args_directory
-    else:
-        directories = get_directories(args_directory)
-        if len(directories) == 0:
-            print('Directory not found', file=sys.stderr)
-            return 1
-        elif len(directories) > 1:
-            print('Directory is not unique', file=sys.stderr)
-            return 1
+    directories = get_directories(args_directory, single=True)
+    if directories is None:
+        return 1
 
-        directory = directories[0]['id']
+    directory = directories['id']
+    directory_name = directories['name']
 
     headers = { 'Content-Type': 'application/json' }
     contents = {
@@ -50,6 +48,30 @@ def sw2_site_add(args):
         return 1
 
     site = json.loads(res.text)
-    print(str(site["id"]))
+
+    messages_diff = []
+    messages = update_site_resources(site['id'], push=True, initial=True)
+    for message in messages:
+        if message['op'] in '+-':
+            messages_diff.append(message)
+
+        site_info = get_sites(site['id'], single=True)
+        if site_info:
+            site = site_info
+        else:
+            site['name'] = args_name
+            site['uri'] = args_uri
+            site['directory_name'] = directory_name
+
+    if args_json: 
+        print(json.dumps({
+            'site': site,
+            'resources': messages_diff
+        }))
+    else:
+        print(str(site['id']), site['name'], site['directory_name'], site["uri"], sep=args_delimiter)
+        for message in messages:
+            if message['op'] in '+-':
+                print(message['op'], message['message'])
 
     return 0
