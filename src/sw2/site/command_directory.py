c95e1700667f91aa2eed02bf1a10f4181755edd8
Miki Yutani <mkyutani@gmail.com>
2024-05-12 23:34:42 +0900

    Change directory by name

--- src/sw2/site/command_directory.py
+++ src/sw2/site/command_directory.py
@@ -3,21 +3,26 @@ from urllib.parse import urljoin
 import requests
 from sw2.directory.list import get_directories, list_directories
 from sw2.env import Environment
+from sw2.site.list import get_sites
 from sw2.util import is_uuid
 
 def sw2_parser_site_directory(subparser):
     parser = subparser.add_parser('directory', help='change directory of site')
-    parser.add_argument('id', metavar='ID', help='id')
+    parser.add_argument('name', help='site id, name or "all"')
     parser.add_argument('directory', metavar='DIR', help='directory id or name')
+    parser.add_argument('--strict', action='store_true', help='site name strict mode')
+    parser.add_argument('--strict-directory', action='store_true', help='directory name strict mode')
 
 def sw2_site_directory(args):
-    args_id = args.get('id')
+    args_name = args.get('name')
     args_directory = args.get('directory')
+    args_strict = args.get('strict')
+    args_strict_directory = args.get('strict_directory')
 
     if is_uuid(args_directory):
         directory = args_directory
     else:
-        directories = list_directories(args_directory)
+        directories = list_directories(args_directory, strict=args_strict_directory)
         if directories is None:
             return 1
         if len(directories) == 0:
@@ -29,22 +34,39 @@ def sw2_site_directory(args):
 
         directory = directories[0]['id']
 
-    headers = { 'Content-Type': 'application/json' }
-    contents = {
-        'directory': directory
-    }
-    query = urljoin(Environment().apiSites(), args_id)
+    if is_uuid(args_name):
+        ids = [args_name]
+    else:
+        sites = get_sites(args_name, strict=args_strict)
+        if sites is None:
+            return 1
+        elif len(sites) == 0:
+            print('site not found', file=sys.stderr)
+            return 1
 
-    res = None
-    try:
-        res = requests.put(query, json=contents, headers=headers)
-    except Exception as e:
-        print(str(e), file=sys.stderr)
+        ids = [s['id'] for s in sites]
+
+    failures = 0
+    for id in ids:
+        headers = { 'Content-Type': 'application/json' }
+        contents = {
+            'directory': directory
+        }
+        query = urljoin(Environment().apiSites(), id)
+
+        res = None
+        try:
+            res = requests.put(query, json=contents, headers=headers)
+        except Exception as e:
+            print(str(e), file=sys.stderr)
+            failures = failures + 1
+
+        if res.status_code >= 400:
+            message = ' '.join([str(res.status_code), res.text if res.text is not None else ''])
+            print(f'{message} ', file=sys.stderr)
+            failures = failures + 1
+
+    if failures > 0:
         return 1
-
-    if res.status_code >= 400:
-        message = ' '.join([str(res.status_code), res.text if res.text is not None else ''])
-        print(f'{message} ', file=sys.stderr)
-        return 1
-
-    return 0
+    else:
+        return 0
