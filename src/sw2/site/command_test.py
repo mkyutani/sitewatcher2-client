db4e9aeb706016f713a0cd4d9437bb7114a4b04c
Miki Yutani <mkyutani@gmail.com>
2024-08-02 18:23:14 +0900

    -djy on testing sites

--- src/sw2/site/command_test.py
+++ src/sw2/site/command_test.py
@@ -1,27 +1,36 @@
 import json
+import sys
+import yaml
+
 from sw2.site.link_list import get_list_links
 
 def sw2_parser_site_test(subparser):
     aliases = []
     parser = subparser.add_parser('test', aliases=aliases, help='test links from uri')
     parser.add_argument('uri', help='url')
-    parser.add_argument('--json', action='store_true', help='in json format')
-    parser.add_argument('--delimiter', nargs=1, default=[' '], help='delimiter')
+    format_group = parser.add_mutually_exclusive_group()
+    format_group.add_argument('-d', '--detail', action='store_true', help='show detail')
+    format_group.add_argument('-j', '--json', action='store_true', help='in json format')
+    format_group.add_argument('-y', '--yaml', action='store_true', help='in yaml format')
     return aliases
 
 def sw2_site_test(args):
     args_directory = args.get('uri')
+    args_detail = args.get('detail')
     args_json = args.get('json')
-    args_delimiter = args.get('delimiter')[0]
+    args_yaml = args.get('yaml')
 
     links = get_list_links(args_directory)
 
     if args_json:
-        print(json.dumps(links))
+        json.dump(links, sys.stdout)
+    elif args_yaml:
+        yaml.dump(links, sys.stdout)
     else:
         for link in links:
             print(link['uri'], link['name'])
-            for key in link['properties'].keys():
-                print('-', key, link['properties'][key], sep=args_delimiter)
+            if args_detail:
+                for key in link['properties'].keys():
+                    print(f'- {key} {link["properties"][key]}')
 
     return 0
