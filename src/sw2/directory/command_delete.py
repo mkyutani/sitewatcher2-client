d9486a7c857bef73e6696ef6d1dd992e03263a22
Miki Yutani <mkyutani@gmail.com>
2024-08-02 17:48:38 +0900

    Remove site/delete.py and directory/delete.py

--- src/sw2/directory/command_delete.py
+++ src/sw2/directory/command_delete.py
@@ -1,39 +1,46 @@
+import json
 import sys
+from urllib.parse import urljoin
+import requests
 
 from sw2.directory.delete import delete_directory
 from sw2.directory.list import get_directories
-from sw2.util import is_uuid
+from sw2.env import Environment
 
 def sw2_parser_directory_delete(subparser):
     parser = subparser.add_parser('delete', help='add directory')
-    parser.add_argument('name', help='directory id, name or "all"')
-    parser.add_argument('--strict', action='store_true', help='directory name strict mode')
+    parser.add_argument('name', nargs='?', metavar='NAME', default=None, help='directory id, name or "all"')
+    parser.add_argument('--strict', action='store_true', help='strict name check')
     return []
 
 def sw2_directory_delete(args):
     args_name = args.get('name')
     args_strict = args.get('strict')
 
-    if is_uuid(args_name):
-        ids = [args_name]
-    else:
-        directories = get_directories(args_name, strict=args_strict)
-        if directories is None:
-            return 1
-        elif len(directories) == 0:
-            print('directory not found', file=sys.stderr)
-            return 1
-
-        ids = [s['id'] for s in directories]
-
-    failures = 0
-    for id in ids:
-        result = delete_directory(id)
-        if result is False:
-            print(f'failed to delete {id}', file=sys.stderr)
-            failures = failures + 1
-
-    if failures > 0:
+    directories = get_directories(args_name, strict=args_strict)
+    if directories is None:
         return 1
-    else:
-        return 0
+    elif len(directories) == 0:
+        print('directory not found', file=sys.stderr)
+        return 1
+
+    for directory in directories:
+        headers = {}
+        query = urljoin(Environment().apiDirectories(), directory['id'])
+
+        res = None
+        try:
+            res = requests.delete(query, headers=headers)
+        except Exception as e:
+            print(str(e), file=sys.stderr)
+            return 1
+
+        if res.status_code >= 400:
+            message = ' '.join([str(res.status_code), res.text if res.text is not None else ''])
+            print(message, file=sys.stderr)
+            return 1
+
+        deleted = json.loads(res.text)
+        print(deleted["id"])
+
+    return 0
--- src/sw2/site/command_delete.py
+++ src/sw2/site/command_delete.py
@@ -1,39 +1,45 @@
+import json
 import sys
+from urllib.parse import urljoin
+import requests
 
-from sw2.util import is_uuid
+from sw2.env import Environment
 from sw2.site.list import get_sites
-from sw2.site.delete import delete_site
 
 def sw2_parser_site_delete(subparser):
     parser = subparser.add_parser('delete', help='add site')
-    parser.add_argument('name', help='site id, name or "all"')
-    parser.add_argument('--strict', action='store_true', help='site name strict mode')
+    parser.add_argument('name', nargs='?', metavar='NAME', default=None, help='site id, name or "all"')
+    parser.add_argument('--strict', action='store_true', help='strict name check')
     return []
 
 def sw2_site_delete(args):
     args_name = args.get('name')
     args_strict = args.get('strict')
 
-    if is_uuid(args_name):
-        ids = [args_name]
-    else:
-        sites = get_sites(args_name, strict=args_strict)
-        if sites is None:
-            return 1
-        elif len(sites) == 0:
-            print('site not found', file=sys.stderr)
-            return 1
-
-        ids = [s['id'] for s in sites]
-
-    failures = 0
-    for id in ids:
-        result = delete_site(id)
-        if result is False:
-            print('failed to delete {id}', file=sys.stderr)
-            failures = failures + 1
-
-    if failures > 0:
+    sites = get_sites(args_name, strict=args_strict)
+    if sites is None:
         return 1
-    else:
-        return 0
+    elif len(sites) == 0:
+        print('site not found', file=sys.stderr)
+        return 1
+
+    for site in sites:
+        headers = {}
+        query = urljoin(Environment().apiSites(), site['id'])
+
+        res = None
+        try:
+            res = requests.delete(query, headers=headers)
+        except Exception as e:
+            print(str(e), file=sys.stderr)
+            return 1
+
+        if res.status_code >= 400:
+            message = ' '.join([str(res.status_code), res.text if res.text is not None else ''])
+            print(message, file=sys.stderr)
+            return 1
+
+        deleted = json.loads(res.text)
+        print(deleted["id"])
+
+    return 0
