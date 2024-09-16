2ba362eb027332279b67e5a4d6bb6c4b0f793b3d
Miki Yutani <mkyutani@gmail.com>
2024-09-16 17:48:16 +0900

    Allow site name as 'directory:site' (low performance because of multiple get_site())

--- src/sw2/site/list.py
+++ src/sw2/site/list.py
@@ -4,6 +4,7 @@ import re
 from urllib.parse import urljoin
 import requests
 import sys
+from sw2.directory.list import list_directories
 from sw2.env import Environment
 from sw2.util import is_uuid
 
@@ -66,15 +67,44 @@ def get_sites(name, strict=False):
         else:
             sites.append(site)
     else:
-        sites = []
-        site_id_names = list_sites(name, strict=strict)
-        if site_id_names is None:
-            return None
-        for id_name in site_id_names:
-            site = get_site(id_name['id'])
-            if site is None:
+        if name and name.find(':') >= 0:
+            directory_name, site_name = name.split(':')
+
+            if len(directory_name) == 0:
+                directory_name = None
+            directory_id_names = list_directories(directory_name, strict=strict)
+            if directory_id_names is None:
                 return None
-            else:
-                sites.append(site)
+
+            if len(site_name) == 0:
+                site_name = None
+            site_id_names = list_sites(site_name, strict=strict)
+            if site_id_names is None:
+                return None
+
+            sites = []
+            for site_id_name in site_id_names:
+                site = get_site(site_id_name['id'])
+                if site is None:
+                    pass
+                elif directory_id_names is not None:
+                    for directory_id_name in directory_id_names:
+                        if site['directory']['id'] == directory_id_name['id']:
+                            sites.append(site)
+                else:
+                    sites.append(site)
+            if len(sites) == 0:
+                return None
+        else:
+            sites = []
+            site_id_names = list_sites(name, strict=strict)
+            if site_id_names is None:
+                return None
+            for id_name in site_id_names:
+                site = get_site(id_name['id'])
+                if site is None:
+                    return None
+                else:
+                    sites.append(site)
 
     return sites
